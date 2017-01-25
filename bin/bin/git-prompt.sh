a7e191bf9333fd3de1f533155e36caffb8b93f76
Isma Perez <pb.ismael@gmail.com>
2017-01-25 17:13:23 +0000

    git branch in prompt script

---
+++ bin/bin/git-prompt.sh
@@ -0,0 +1,20 @@
+#Display git branch on prompt
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+function proml {
+  local        BLUE="\[\033[0;34m\]"
+  local         RED="\[\033[0;31m\]"
+  local   LIGHT_RED="\[\033[1;31m\]"
+  local       GREEN="\[\033[0;32m\]"
+  local LIGHT_GREEN="\[\033[1;32m\]"
+  local       WHITE="\[\033[1;37m\]"
+  local  LIGHT_GRAY="\[\033[0;37m\]"
+  local     DEFAULT="\[\033[0m\]"
+
+  export PS1="\u@\h:$GREEN\W $RED\$(parse_git_branch)$DEFAULT\$ "
+  export PROMPT_COMMAND="echo -ne \"\033]0;\$(parse_git_branch)\${PWD##*/}\007\""
+}
+
+proml
