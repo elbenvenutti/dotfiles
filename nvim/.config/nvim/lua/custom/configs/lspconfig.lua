e0e08b70b12a87150464d3f7340fd94d7a972e5c
Isma Perez <pb.ismael@gmail.com>
2024-03-26 11:32:25 +0000

    add tailwind to lspconfig

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "metals", "tsserver", "eslint"}
+local servers = { "html", "cssls", "metals", "tsserver", "eslint", "tailwindcss", "marksman"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
