cd8278a3964f63630a27835313c9167d662588b3
Isma Perez <pb.ismael@gmail.com>
2024-08-15 08:37:45 +0100

    don't enable tailwind lsp on postcss presence

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "metals", "tsserver", "eslint", "tailwindcss", "marksman"}
+local servers = { "html", "cssls", "metals", "tsserver", "eslint", "marksman"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -10,6 +10,20 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig["tailwindcss"].setup {
+  hovers = true,
+  suggestions = true,
+  root_dir = function(fname)
+    local root_pattern = require("lspconfig").util.root_pattern(
+      "tailwind.config.mjs",
+      "tailwind.config.cjs",
+      "tailwind.config.js"
+      -- "postcss.config.js"
+    )
+    return root_pattern(fname)
+  end
+}
 
 vim.api.nvim_create_autocmd(
   {"BufWritePre"},
